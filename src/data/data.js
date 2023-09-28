0445b60561f236cb79c29ae9edce3041c199224b
whitefantom00 <51983408+whitefantom00@users.noreply.github.com>
2023-09-29 00:42:13 +0700

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -3,18 +3,12 @@
 export const data = [
 
   {
-    id: 2,
+    id: 1,
     img: 'https://i.postimg.cc/Df5JmCw6/Screenshot-2023-09-23-204409.png',
     title: "System Analysis",
     desc: "",
     link: "https://www.canva.com/design/DAFlreNGXPc/yPr5FKUJR_njdtj3YVuszQ/edit?utm_content=DAFlreNGXPc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
     git: "",
   },
-  {
-    id: 1,
-    img: 'https://i.postimg.cc/2yn0FP91/download.png',
-    title: "Data Analysis",
-    link: "https://colab.research.google.com/drive/1k1EUAEZxKyaClfWcIAF3IFzvva1iAkFz",
-    git: "",
-  },
+
 ];
