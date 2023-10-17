fff0c75ca2cc4e5f120fbf63021ef962f60db276
whitefantom00 <51983408+whitefantom00@users.noreply.github.com>
2023-10-17 16:11:13 +0700

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,7 @@ export const data = [
   
   {
     id: 1,
-    img: 'https://i.postimg.cc/Df5JmCw6/Screenshot-2023-09-23-204409.png',
+    img: 'https://i.postimg.cc/NFDg23qJ/Desktop-Login.jpg',
     title: "System Analysis",
     desc: "",
     link: "https://www.canva.com/design/DAFlreNGXPc/yPr5FKUJR_njdtj3YVuszQ/edit?utm_content=DAFlreNGXPc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
