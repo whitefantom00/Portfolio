769dbd86b58d3e11112c43cb27470eaef7716193
whitefantom00 <51983408+whitefantom00@users.noreply.github.com>
2023-10-21 14:19:00 +0700

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,7 @@ export const data = [
   
   {
     id: 1,
-    img: 'https://i.postimg.cc/NFDg23qJ/Desktop-Login.jpg',
+    img: 'https://i.postimg.cc/mkHyWqyz/Desktop-Login.jpg',
     title: "System Analysis",
     desc: "",
     link: "https://www.canva.com/design/DAFlreNGXPc/yPr5FKUJR_njdtj3YVuszQ/edit?utm_content=DAFlreNGXPc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
@@ -12,7 +12,7 @@ export const data = [
   },
   {
     id: 2,
-    img: 'https://i.postimg.cc/1zyrF8Rt/download.png',
+    img: 'https://i.postimg.cc/mkHyWqyz/Desktop-Login.jpg',
     title: "Data Preprocessing and DSA",
     desc: "",
     link: "https://colab.research.google.com/drive/1k1EUAEZxKyaClfWcIAF3IFzvva1iAkFz#scrollTo=Gm9G1C15B83Y",
@@ -20,7 +20,7 @@ export const data = [
   },
   {
     id: 3,
-    img: 'https://i.postimg.cc/1zyrF8Rt/download.png',
+    img: 'https://i.postimg.cc/mkHyWqyz/Desktop-Login.jpg',
     title: "Data Preprocessing and DSA",
     desc: "",
     link: "https://colab.research.google.com/drive/1oNyvcJdtGZgC3YGfpzZDw0p5elb41vXY?authuser=1#scrollTo=TzoMXb9pYwQi",
