b1e38d6f4107e0f3c78639a92581621dae55e8eb
whitefantom00 <51983408+whitefantom00@users.noreply.github.com>
2023-10-21 14:15:47 +0700

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -17,6 +17,13 @@ export const data = [
     desc: "",
     link: "https://colab.research.google.com/drive/1k1EUAEZxKyaClfWcIAF3IFzvva1iAkFz#scrollTo=Gm9G1C15B83Y",
     git: "",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/1zyrF8Rt/download.png',
+    title: "Data Preprocessing and DSA",
+    desc: "",
+    link: "https://colab.research.google.com/drive/1oNyvcJdtGZgC3YGfpzZDw0p5elb41vXY?authuser=1#scrollTo=TzoMXb9pYwQi",
+    git: "",
   }
-  
 ];
