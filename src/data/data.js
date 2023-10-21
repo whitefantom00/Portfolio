abaaa8235b1c6be9fff9362a2fa48c0f5abb62af
whitefantom00 <51983408+whitefantom00@users.noreply.github.com>
2023-10-21 14:32:38 +0700

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -4,26 +4,11 @@ export const data = [
   
   {
     id: 1,
-    img: 'https://i.postimg.cc/44MRs9sp/Desktop-Login.jpg',
+    img: 'https://i.postimg.cc/XX6Jtyp6/Desktop-Login.jpg',
     title: "System Analysis",
     desc: "",
     link: "https://www.canva.com/design/DAFlreNGXPc/yPr5FKUJR_njdtj3YVuszQ/edit?utm_content=DAFlreNGXPc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
     git: "",
   },
-  {
-    id: 2,
-    img: 'https://i.postimg.cc/44MRs9sp/Desktop-Login.jpg',
-    title: "Data Preprocessing and DSA",
-    desc: "",
-    link: "https://colab.research.google.com/drive/1k1EUAEZxKyaClfWcIAF3IFzvva1iAkFz#scrollTo=Gm9G1C15B83Y",
-    git: "",
-  },
-  {
-    id: 3,
-    img: 'https://i.postimg.cc/44MRs9sp/Desktop-Login.jpg',
-    title: "Data Preprocessing and DSA",
-    desc: "",
-    link: "https://colab.research.google.com/drive/1oNyvcJdtGZgC3YGfpzZDw0p5elb41vXY?authuser=1#scrollTo=TzoMXb9pYwQi",
-    git: "",
-  }
+
 ];
