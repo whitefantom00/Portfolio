d82f20bb32a21c971a2fe5574c9cfd6e5689c263
whitefantom00 <51983408+whitefantom00@users.noreply.github.com>
2023-10-21 14:09:31 +0700

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -10,14 +10,13 @@ export const data = [
     link: "https://www.canva.com/design/DAFlreNGXPc/yPr5FKUJR_njdtj3YVuszQ/edit?utm_content=DAFlreNGXPc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
     git: "",
   },
-
-    {
+  {
     id: 2,
     img: 'https://i.postimg.cc/1zyrF8Rt/download.png',
     title: "Data Preprocessing and DSA",
     desc: "",
     link: "https://colab.research.google.com/drive/1k1EUAEZxKyaClfWcIAF3IFzvva1iAkFz#scrollTo=Gm9G1C15B83Y",
     git: "",
-  },
+  }
   
 ];
