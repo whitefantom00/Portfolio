76a1fc965bb1f0c33ba4869dc0d77b641c4fa142
whitefantom00 <51983408+whitefantom00@users.noreply.github.com>
2023-10-24 18:42:17 +0700

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -4,9 +4,9 @@ export const data = [
   
   {
     id: 1,
-    img: 'https://i.postimg.cc/XX6Jtyp6/Desktop-Login.jpg',
+    img: 'https://i.postimg.cc/MHTrbgr1/Desktop-Login.jpg',
     title: "System Analysis",
-    desc: "",
+    desc: "This project is an application that aims to help a retail clothes store to manage their sales. The data used is a sample dataset downloaded from Kaggle.",
     link: "https://www.canva.com/design/DAFlreNGXPc/yPr5FKUJR_njdtj3YVuszQ/edit?utm_content=DAFlreNGXPc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
     git: "",
   },
