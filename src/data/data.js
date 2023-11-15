e8752191978baff88a2be6b05d181f1698eb58eb
whitefantom00 <51983408+whitefantom00@users.noreply.github.com>
2023-11-15 17:18:20 +0700

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -12,7 +12,7 @@ export const data = [
   },
   {
     id: 2,
-    img: 'https://i.postimg.cc/PxLPLRN2/stock-markets-60999aa9.png',
+    img: 'https://i.postimg.cc/Cxj5k8hp/stock-markets-60999aa9.png',
     title: "Stock Price Analysis",
     desc: "This project is an application of LSTM Model to analyze and predict Stock Price of TNG. The data used is collected from CafeF.",
     link: "https://www.kaggle.com/code/vkimmm/tng-and-rivals",
@@ -20,7 +20,7 @@ export const data = [
   },
    {
     id: 3,
-    img: 'https://i.postimg.cc/wMqGHMHm/Screenshot-9.png',
+    img: 'https://i.postimg.cc/5NTW55bK/Screenshot-9.png',
     title: "Complaination Analysis",
     desc: "This project is visualization of complaination of players in 12 different games that published by GAMOTA. The data used is crawled from CH Play.",
     link: "https://husteduvn-my.sharepoint.com/:p:/g/personal/vu_kh210946_sis_hust_edu_vn/ESJd-MmorQZHhjufZA0SZn4BtYHehlfQLyz_foy0HQNPqQ?e=qozxn5",
