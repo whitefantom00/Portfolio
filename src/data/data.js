b51b420f6c23111a77b2e373006aa146d977c616
whitefantom00 <51983408+whitefantom00@users.noreply.github.com>
2023-11-15 17:12:58 +0700

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -10,5 +10,20 @@ export const data = [
     link: "https://www.canva.com/design/DAFlreNGXPc/yPr5FKUJR_njdtj3YVuszQ/edit?utm_content=DAFlreNGXPc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
     git: "",
   },
-
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/PxLPLRN2/stock-markets-60999aa9.png',
+    title: "Stock Price Analysis",
+    desc: "This project is an application of LSTM Model to analyze and predict Stock Price of TNG. The data used is collected from CafeF.",
+    link: "https://www.kaggle.com/code/vkimmm/tng-and-rivals",
+    git: "https://github.com/whitefantom00/TNG-Stock-Price-Analysis.git",
+  },
+   {
+    id: 3,
+    img: 'https://i.postimg.cc/wMqGHMHm/Screenshot-9.png',
+    title: "Stock Price Analysis",
+    desc: "This project is visualization of complaination of players in 12 different games that published by GAMOTA. The data used is crawled from CH Play.",
+    link: "https://husteduvn-my.sharepoint.com/:p:/g/personal/vu_kh210946_sis_hust_edu_vn/ESJd-MmorQZHhjufZA0SZn4BtYHehlfQLyz_foy0HQNPqQ?e=qozxn5",
+    git: "https://husteduvn-my.sharepoint.com/:u:/g/personal/vu_kh210946_sis_hust_edu_vn/EQL-cgfaG2NJkUHro2QLd78BAOtl9HvxGZNJdK54POVhsA",
+  }, 
 ];
