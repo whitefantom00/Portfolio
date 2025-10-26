ee933d174764de3cac4a5952b9a9bb34ac8fdaea
Vũ Kim <51983408+whitefantom00@users.noreply.github.com>
2025-10-27 00:00:09 +0700

    cm

--- src/data/data.js
+++ src/data/data.js
@@ -1,49 +1,27 @@
 
 
 export const data = [
-  
-  {
+
+   {
     id: 1,
-    img: 'https://i.postimg.cc/MHTrbgr1/Desktop-Login.jpg',
-    title: "System Analysis",
-    desc: "This project is an application that aims to help a retail clothes store to manage their sales. The data used is a sample dataset downloaded from Kaggle.",
-    link: "https://www.canva.com/design/DAFlreNGXPc/yPr5FKUJR_njdtj3YVuszQ/edit?utm_content=DAFlreNGXPc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
-    git: "",
-  },
+    img: 'https://i.postimg.cc/k4VKqyMG/Screenshot-2025-10-26-233415.png',
+    title: "General Marketing Report",
+    desc: "This project is a general marketing report including sales, customers, products, and operation analysis. Data is mock data generated using Python.",
+    link: "https://drive.google.com/file/d/1VAVzMaymRztfUvPFkd-aA4wR1T4SVpZU/view?usp=drive_link",
+    git: "https://drive.google.com/file/d/1L-zBwg2yrqOM81EE6iL1pwzG5tiQOnl4/view?usp=sharing",
+  }, 
+
   {
     id: 2,
-    img: 'https://i.postimg.cc/Cxj5k8hp/stock-markets-60999aa9.png',
-    title: "Stock Price Analysis",
-    desc: "This project is an application of LSTM Model to analyze and predict Stock Price of TNG. The data used is collected from CafeF.",
-    link: "https://www.kaggle.com/code/vkimmm/tng-and-rivals/notebook",
-    git: "https://github.com/whitefantom00/TNG-Stock-Price-Analysis.git",
-  },
-   {
-    id: 3,
-    img: 'https://i.postimg.cc/Z59D4Fsb/Screenshot-2024-02-20-223402.png',
-    title: "Internet Ads Analysis",
-    desc: "This project is a visualization of Internet Ads efficiency.",
-    link: "https://app.powerbi.com/view?r=eyJrIjoiMzczYjg5YzEtMWVhMi00Njc4LWJmZmYtNWYxMjY1NGE4MDY3IiwidCI6IjQxYWI0MmE5LTM4MWItNDhjZi04YTg1LTcyMDQ2NDkyMjk3NiIsImMiOjEwfQ%3D%3D",
-    git: "https://app.powerbi.com/view?r=eyJrIjoiMzczYjg5YzEtMWVhMi00Njc4LWJmZmYtNWYxMjY1NGE4MDY3IiwidCI6IjQxYWI0MmE5LTM4MWItNDhjZi04YTg1LTcyMDQ2NDkyMjk3NiIsImMiOjEwfQ%3D%3D",
-  }, 
-  {
-    id: 4,
     img: 'https://i.postimg.cc/G3KL6Y5g/Screenshot-2024-02-20-223857.png',
     title: "Investment Funds Analysis",
     desc: "This project is a report of a fund management company and 2 funds that they manage.",
     link: "https://app.powerbi.com/view?r=eyJrIjoiODliZWU4ZjUtYjE5NS00M2I2LWJiMzgtMWIxYTQ5MzBlMTJiIiwidCI6IjQxYWI0MmE5LTM4MWItNDhjZi04YTg1LTcyMDQ2NDkyMjk3NiIsImMiOjEwfQ%3D%3D",
     git: "https://app.powerbi.com/view?r=eyJrIjoiODliZWU4ZjUtYjE5NS00M2I2LWJiMzgtMWIxYTQ5MzBlMTJiIiwidCI6IjQxYWI0MmE5LTM4MWItNDhjZi04YTg1LTcyMDQ2NDkyMjk3NiIsImMiOjEwfQ%3D%3D",
   }, 
-   {
-    id: 5,
-    img: 'https://i.postimg.cc/X7Js8X8r/output.png',
-    title: "Customer EDA Analysis",
-    desc: "This project is about Customer EDA and Clustering. The data used is a sample dataset downloaded from Kaggle.",
-    link: "",
-    git: "https://github.com/whitefantom00/Customer-EDA-Report.git",
-  }, 
+
   {
-    id: 6,
+    id: 3,
     img: 'https://i.postimg.cc/dQBKdrVM/Overview.png',
     title: "ETF Management",
     desc: "This project is about ETF fund management",
